02f29534c3d937524f191da0f4aba92651389f77
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-05-02 20:04:41 +0300

    init

--- FaceAuthenticationInSwiftUI/ContentView.swift
+++ FaceAuthenticationInSwiftUI/ContentView.swift
@@ -6,11 +6,48 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct ContentView: View {
+    var context = LAContext()
+    
+    init() {
+        context.canEvaluatePolicy(.deviceOwnerAuthentication, error: nil)
+    }
+    
+    @State private var isLogged: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: {
+            
+        }, label: {
+            Text(isLogged ? "Logout" : "Login")
+                .font(.largeTitle)
+        })
+    }
+    
+    private func login() {
+        if !isLogged {
+            var error: NSError?
+            if context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error) {
+                let reason = "Login into the application"
+                context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reason) { success, error in
+                    if success {
+                        DispatchQueue.main.async {
+                            isLogged = true
+                        }
+                    } else {
+                        print("Error \(error?.localizedDescription ?? "")")
+                    }
+                }
+            } else {
+                print("Error found \(error?.localizedDescription ?? "Nothing found")")
+            }
+        } else {
+            DispatchQueue.main.async {
+                isLogged = false
+            }
+        }
     }
 }
 
