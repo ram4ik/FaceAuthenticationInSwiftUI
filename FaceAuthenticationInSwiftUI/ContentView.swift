eb3f421dd5082e979ce5927e283308106a995218
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-05-02 19:46:31 +0300

    Initial Commit

---
+++ FaceAuthenticationInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FaceAuthenticationInSwiftUI
+//
+//  Created by ramil on 02.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FaceAuthenticationInSwiftUI/FaceAuthenticationInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceAuthenticationInSwiftUIApp.swift
+//  FaceAuthenticationInSwiftUI
+//
+//  Created by ramil on 02.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct FaceAuthenticationInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
